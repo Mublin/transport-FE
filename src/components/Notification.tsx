6c2bb75bc8e6eec030f2cf8c4ea5b8c9b0f2e69f
[mubleen] <102492190+Mublin@users.noreply.github.com>
2025-06-22 18:23:09 +0100

    toast notifications

---
+++ src/components/Notification.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const Notification: React.FC = () => {
+  return null // Notification is handled in NotificationContext
+}
+
+export default Notification
